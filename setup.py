d273f6a5fb343a6e7c1ffc670390c1c7164ea0e4
Vinícius Mendes <vbmendes@gmail.com>
2010-08-23 21:27:03 -0300

    Improvements in setup.py
    Adding long_description, author_email and classifiers.

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 from setuptools import setup, find_packages
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name = "django-meio-shorturl",
     version = "1.0",
     url = 'http://github.com/vbmendes/django-meio-shorturl',
     license = 'BSD',
     description = "Short URL handling in Django apps.",
+    long_description = read('README'),
     author = 'Vinicius Mendes',
+    author_email = 'vbmendes@gmail.com',
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires = ['setuptools'],
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
 
